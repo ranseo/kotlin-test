278779d4b8f6c9fdd58862620a8c36a1c870a85b
ranseo <55490534+ranseo@users.noreply.github.com>
2021-10-15 13:11:50 +0900

    드론 성공

--- src/programmers/MoveBlock.kt
+++ src/programmers/MoveBlock.kt
@@ -4,14 +4,15 @@ import java.util.*
 
 //typealias P = Pair<Int, Int>
 typealias P2 = Pair<Array<P>, Int>
+typealias T1 =Triple<Array<P>, Int, MutableList<String>>
 
 fun P.plus(v: P): P = P(this.first + v.first, this.second + v.second)
 fun P.minus(v: P): P = P(this.first - v.first, this.second - v.second)
-
+fun P.compare(v:P): Boolean = (this.first-v.first > 0 || this.second - v.second >0 )
 
 class SolutionMoveBlock {
     val visit = hashMapOf<String, Boolean>()
-
+    var visitRoute = mutableListOf<String>()
     //아래,위,오른쪽,왼쪽
     val direction = arrayOf(P(1, 0), P(-1, 0), P(0, 1), P(0, -1))
 
@@ -32,9 +33,11 @@ class SolutionMoveBlock {
 
     //BFS.
     fun BFS(board: Array<IntArray>, drone: Array<P>) {
-        var queue: ArrayList<P2> = arrayListOf()
-        visit(drone)
-        queue.add(P2(drone, 0))
+        //var queue: ArrayList<P2> = arrayListOf()
+        var queue: ArrayList<T1> = arrayListOf()
+        Visit(drone)
+
+        queue.add(T1(drone, 0, mutableListOf()))
 
 
 
@@ -42,54 +45,82 @@ class SolutionMoveBlock {
         while (queue.isNotEmpty()) {
             var start = queue.first().first
             var count = queue.first().second
+            var route = queue.first().third
+            var FinalRoute = ""
+
 
             println("start : ${start[0]}  ${start[1]}, count : $count")
 
+
             if ((start[0].first == n - 1) && (start[0].second == n - 1)) {
+                FinalRoute = "${start[0]}, ${start[1]} and Count : ${count}"
                 answer = Math.min(answer, count)
+                print(route+FinalRoute )
+                println()
                 break
             }
             if ((start[1].first == n - 1) && (start[1].second == n - 1)) {
+                FinalRoute= "${start[0]}, ${start[1]} and Count : ${count}"
                 answer = Math.min(answer, count)
+                print(route+FinalRoute )
+                println()
                 break
             }
 
             queue.removeAt(0)
             for (i in 0 until 4) {
-                var next = move(start, direction[i])
+                var next = move(start.clone(), direction[i])
 
                 if (checkVisit(next)) continue
                 if (!checkOutBoard(next[0]) || !checkOutBoard(next[1])) continue
                 if (!checkCurrLoc(next, board)) continue
 
 
-                visit(next)
-                queue.add(P2(next, count + 1))
+                Visit(next)
+                val tmp = route.toMutableList()
+                var visitRoute = "[Move!] ${start[0]}, ${start[1]} and Count : ${count} ->"
+                tmp.add(visitRoute)
+                queue.add(T1(next, count + 1, tmp))
             }
 
 
             for (i in 0..1) {
-                var next = start
-                for (j in 0..1) {
-                    next = start
-                    for (k in 0 until 4) {
-                        next = rotate(board, next, j, i)
+                    var next1 = rotate(board, start.clone(), i, 0)
 
-                        if (checkVisit(next)) continue
-                        if (!checkOutBoard(next[0]) || !checkOutBoard(next[1])) continue
-                        if (!checkCurrLoc(next, board)) continue
+                    if(next1.size < 1) continue
+                    if (checkVisit(next1)) continue
+                    if (!checkOutBoard(next1[0]) || !checkOutBoard(next1[1])) continue
+                    if (!checkCurrLoc(next1, board)) continue
 
 
-                        visit(next)
-                        queue.add(P2(next, count + 1))
+                    Visit(next1)
+                    val tmp = route.toMutableList()
+                    var visitRoute = "[반시계 회전!] ${start[0]}, ${start[1]} and Count : ${count}  ->"
+                    tmp.add(visitRoute)
+                    queue.add(T1(next1, count + 1,tmp))
 
-                    }
-                }
             }
+
+
+            for (i in 0..1) {
+                    var next2 = rotate(board, start.clone(), i, 1)
+
+                    if(next2.size < 1) continue
+                    if (checkVisit(next2)) continue
+                    if (!checkOutBoard(next2[0]) || !checkOutBoard(next2[1])) continue
+                    if (!checkCurrLoc(next2, board)) continue
+
+
+                    Visit(next2)
+                    val tmp = route.toMutableList()
+                    var visitRoute = "[시계 회전!] ${start[0]}, ${start[1]} and Count : ${count} ->"
+                    tmp.add(visitRoute)
+                    queue.add(T1(next2, count + 1,tmp))
+            }
+
         }
-
-
     }
+
 
     //움직이게 만드는 함수.
     fun move(drone: Array<P>, direction: P): Array<P> = arrayOf(drone[0].plus(direction), drone[1].plus(direction))
@@ -123,7 +154,7 @@ class SolutionMoveBlock {
         return false
     }
 
-    fun visit(drone: Array<P>) {
+    fun Visit(drone: Array<P>) {
         val key1 = "${drone[0].first},${drone[0].second}+${drone[1].first},${drone[1].second}"
         val key2 = "${drone[1].first},${drone[1].second}+${drone[0].first},${drone[0].second}"
         if (!visit.containsKey(key1)) visit.put(key1, true)
@@ -132,6 +163,9 @@ class SolutionMoveBlock {
 
 
     //90도 (반시계or시계) 방향으로 회전 시키는 함수. 반시계는 direction = 0 // 시계는 direction = 1
+    //val rotationValue = arrayOf(P(1, 1), P(-1, 1), P(-1, -1), P(1, -1))
+    //가운데 기준 - 왼쪽, 위, 오른쪽, 아래
+    //val location = arrayOf(P(0, -1), P(1, 0), P(0, 1), P(-1, 0))
     fun rotate(board: Array<IntArray>, drone: Array<P>, center: Int, clockwise: Int): Array<P> {
         val rotation = if (center == 0) 1 else 0
 
@@ -144,7 +178,7 @@ class SolutionMoveBlock {
                     rotate = if (clockwise == 0) drone[rotation].plus(rotationValue[0]) else drone[rotation].plus(
                         rotationValue[1]
                     )
-                } else return drone
+                } else return arrayOf()
             }
 
             //아래
@@ -153,7 +187,7 @@ class SolutionMoveBlock {
                     rotate = if (clockwise == 0) drone[rotation].plus(rotationValue[1]) else drone[rotation].plus(
                         rotationValue[2]
                     )
-                } else return drone
+                } else return arrayOf()
             }
 
             //오른쪽
@@ -162,24 +196,23 @@ class SolutionMoveBlock {
                     rotate = if (clockwise == 0) drone[rotation].plus(rotationValue[2]) else drone[rotation].plus(
                         rotationValue[3]
                     )
-                } else return drone
+                } else return arrayOf()
             }
 
-            //위쪽
+            //위
             location[3] -> {
                 if (checkRotateUp(board, drone[rotation], clockwise)) {
                     rotate = if (clockwise == 0) drone[rotation].plus(rotationValue[3]) else drone[rotation].plus(
                         rotationValue[0]
                     )
-                } else return drone
+                } else return arrayOf()
             }
         }
 
-        return if (center == 0) arrayOf(drone[center], rotate) else arrayOf(rotate, drone[center])
-
-
+        return if (rotate.compare(drone[center])) arrayOf(drone[center], rotate) else arrayOf(rotate, drone[center])
     }
 
+    //
     //val direction = arrayOf(P(1, 0), P(-1, 0), P(0, 1), P(0, -1))
     fun checkRotateLeft(board: Array<IntArray>, rotation: P, clockwise: Int): Boolean {
         var tmp: P
