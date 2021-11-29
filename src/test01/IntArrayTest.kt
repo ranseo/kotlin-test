de258a11841469cddbfb4430a5b9366f59d689a9
ranseo <slcksseo@gmail.com>
2021-11-29 22:00:22 +0900

    StringToList 는 split야 잊지마~~

---
+++ src/test01/IntArrayTest.kt
@@ -0,0 +1,18 @@
+package test01
+
+import kotlin.time.Duration.Companion.seconds
+
+typealias P3 = Pair<Int,Int>
+fun main() {
+    val intarray = intArrayOf(1,2,3,4)
+
+    val list = mutableListOf<P3>(P3(1,1), P3(2,4), P3(2,5))
+
+    println(intarray.maxByOrNull { it.seconds })
+
+
+}
+
+fun MutableList<Int>.initList() {
+
+}
---
+++ src/test01/StringToList.kt
@@ -0,0 +1,9 @@
+package test01
+
+fun main() {
+    val str = "abc ab addd"
+
+    val list = str.split(" ")
+
+    println(list)
+}
